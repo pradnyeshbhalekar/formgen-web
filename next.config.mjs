0368b96985caa5ef52943e16f60e4023d045b1db
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2025-07-25 20:14:50 +0530

    fix: update postcss.config and next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'export'
+  // swcMinify: true,  <-- REMOVE this if you're using Next 15
+  experimental: {
+    serverActions: true,
+  },
 };
 
 export default nextConfig;
