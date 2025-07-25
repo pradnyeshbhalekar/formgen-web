8227c5669e34df2c0b620cb4053664445b8d2da3
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2025-07-25 21:05:42 +0530

    Add render.yaml config for frontend and backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
-export default {
-  experimental: {
-    serverActions: {}
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 👈 Required for static export in Next.js 13+
 };
+
+export default nextConfig;
